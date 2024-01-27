097b1b0d4f8f654500861295e2fd898c67b3a39f
AmigoDmitriy <151064884+AmigoDmitriy@users.noreply.github.com>
2024-01-27 06:12:11 +0200

    sdfsd

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,10 @@
 function getElementWidth(content, padding, border) {
-  return content + padding * 2 + border * 2;
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat(border);
+  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  return totalWidth;
 }
-
-getElementWidth(50, 8, 4);
-getElementWidth(60, 12, 8.5);
-getElementWidth(200, 0, 0);
 
 console.log(getElementWidth('50px', '8px', '4px')); // 74
 console.log(getElementWidth('60px', '12px', '8.5px')); // 101
