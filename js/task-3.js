a6742333b5409e3f3af0a419d393250f5a5a7171
AmigoDmitriy <151064884+AmigoDmitriy@users.noreply.github.com>
2024-01-27 06:18:35 +0200

    bnmbnm

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
   const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+  const totalWidth = contentWidth + paddingWidth * 2 + borderWidth * 2;
   return totalWidth;
 }
 
